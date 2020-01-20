c86f0a2833cd223302f034fb8cf14490cd0a9793
Sorru <Conatrad@gmail.com>
2020-01-19 19:20:42 -0500

    Adjusted when header gets pinned

--- js/script.js
+++ js/script.js
@@ -1,7 +1,9 @@
 let header = document.querySelector("#header");
-let sticky = header.offsetTop;
+let phrase = document.querySelector(".phrase");
+let sticky = phrase.offsetTop;
 let logo = document.querySelector(".logo");
 
+//Pins header to top of page when scrolling past home screen
 window.onscroll = () => window.pageYOffset > sticky ? header.classList.add("sticky") : header.classList.remove("sticky");
 
 logo.addEventListener("click", window.scroll(0, 0));
